fc7e6ea04bf5a3a81a74fc40959e2928f58b7066
SruthiKuppili98 <72158206+SruthiKuppili98@users.noreply.github.com>
2020-11-03 09:14:29 +0530

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Student
+(
+	[StudentId] INT NULL,
+	[StudentName] nvarchar(50) null,
+	[StudentEmail-ID] nvarchar(50) null,
+	[StudentMobileNo] Bigint null,
+	[StudentGender] nvarchar(50) null,
+	[StudentCity] nvarchar(50) null,
+	[StudentAddress] nvarchar(50) null,
+	[StudentImg] nvarchar(50) null,
+
+
+)

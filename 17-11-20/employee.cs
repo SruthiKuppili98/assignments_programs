ed64a9861445ed5d86c62ab3802732ea15a859e9
sruthi <72158206+SruthiKuppili98@users.noreply.github.com>
2020-11-17 23:07:38 +0530

    Add files via upload

---
+++ 17-11-20/employee.cs
@@ -0,0 +1,12 @@
+namespace WebApplication8
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
